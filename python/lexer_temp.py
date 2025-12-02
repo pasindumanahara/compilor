7ceb7df542b2aabd55c43e5eb5d58dddefe4fcc7
pasindumanahara <manahara2003@gmail.com>
2025-12-02 13:24:32 +0530

    edit lexer file

--- python/lexer_temp.py
+++ python/lexer_temp.py
@@ -52,3 +52,171 @@ MULTI_CHAR_TOKENS = {
     '>=': "GE"
 }
 
+# example lexer class helper
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.line = 1
+        self.current_char = self.text[self.pos] if self.text else None
+
+    def advance(self):
+        if self.current_char == '\n':
+            self.line += 1
+        self.pos += 1
+        if self.pos < len(self.text):
+            self.current_char = self.text[self.pos]
+        else:
+            self.current_char = None
+        
+    def peek(self):        
+        next_pos = self.pos + 1
+        if next_pos < len(self.text):
+            return self.text[next_pos]
+        return None
+
+
+    def _id(self):
+        result = ''
+        while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
+            result += self.current_char
+            self.advance()
+
+        if result in KEYWORDS:
+            return Token(result.upper(), result, self.line)  # e.g., IF, ELSE
+        else:
+            return Token("ID", result, self.line)
+
+    def number(self):        
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return Token("INT_LITERAL", int(result), self.line)
+    
+    def skip_whitspaces(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def skip_line_comments(self):
+        self.advance()
+        self.advance()
+        while self.current_char is not None and self.current_char != '\n':
+            self.advance()
+
+    def skip_block_comments(self):
+        self.advance()
+        self.advance()
+        while self.current_char is not None:
+            if self.current_char == '*' and self.peek() == '/':
+                self.advance()
+                self.advance()
+                return
+            else:
+                self.advance()
+        raise Exception(f"Unterminated block comment at line {self.line}")
+    
+    def get_next_token(self):
+        while self.current_char is not None:   
+            # white spaces         
+            if self.current_char.isspace():
+                self.skip_whitspaces()
+                continue
+            
+            # handle comments
+            if self.current_char == '/':
+                next_char = self.peek()
+                if next_char == '/':
+                    self.skip_line_comments()
+                    continue
+                if next_char == '*':
+                    self.skip_block_comments()
+                    continue 
+                self.advance()
+                return Token("DIV",'/',self.line)
+            
+            # identifers and key words
+            if self.current_char.isalpha() or self.current_char == '_':
+                return self.identifier() # TODO
+            
+            if self.current_char.isdigit():
+                return self.number()
+            
+            # operators
+            if self.current_char == '+':
+                self.advance()
+                return Token("PLUS", '+', self.line)
+            if self.current_char == '-':
+                self.advance()
+                return Token("MINUS", '-', self.line)
+            if self.current_char == '*':
+                self.advance()
+                return Token("MUL", '*', self.line)
+            if self.current_char == '=':
+                self.advance()
+                return Token("ASSIGN", '=', self.line)
+            if self.current_char == ';':
+                self.advance()
+                return Token("SEMI", ';', self.line)
+            if self.current_char == '(':
+                self.advance()
+                return Token("LPAREN", '(', self.line)
+            if self.current_char == ')':
+                self.advance()
+                return Token("RPAREN", ')', self.line)
+            
+            # unknown character
+            raise Exception(f"Illegal character '{self.current_char}' at line {self.line}")
+        return Token("EOF", None, self.line)
+
+            
+
+            
+                
+
+            
+
+    
+
+# example for using this functions and the class to check each token 
+'''
+def get_next_token(self):
+    while self.current_char is not None:
+
+        if self.current_char.isspace():
+            self.skip_whitespace()
+            continue
+
+        if self.current_char.isalpha() or self.current_char == '_':
+            return self._id()
+
+        if self.current_char.isdigit():
+            return self.number()
+
+        # handle comments
+        if self.current_char == '/':
+            if self.peek() == '/':
+                self.skip_line_comment()
+                continue
+            if self.peek() == '*':
+                self.skip_block_comment()
+                continue
+            return Token("DIV", '/', self.line)
+
+        # handle operators (==, !=, <=, etc.)
+        token = self.match_operator()
+        if token:
+            return token
+
+        # symbols ((), {}, etc.)
+        if self.current_char in SINGLE_CHAR_TOKENS:
+            ch = self.current_char
+            self.advance()
+            return Token(SINGLE_CHAR_TOKENS[ch], ch, self.line)
+
+        # unknown character → error
+        raise Exception(f"Unexpected char {self.current_char!r} at line {self.line}")
+
+    return Token("EOF", None, self.line)
+
+'''
